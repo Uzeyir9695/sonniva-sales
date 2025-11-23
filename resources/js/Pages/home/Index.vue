9aea77f83d9506ab744dd3d56bef55a36c34d4c0
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2025-11-23 21:39:34 +0400

    testing github deploy

--- resources/js/Pages/home/Index.vue
+++ resources/js/Pages/home/Index.vue
@@ -6,6 +6,8 @@ import {Link} from "@inertiajs/vue3";
 <template>
     <div class="min-h-screen bg-white text-center text-2xl p-6 italic">
        Welcome, Sonniva is coming soon!
-
+        <div class="mt-6 font-bold">
+            We are going to build an awesome application.
+        </div>
     </div>
 </template>
