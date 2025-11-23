7ca3b708369f5f8c3833f62295dd2284747b8a0e
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2025-11-23 17:40:34 +0400

    first commit

--- resources/js/Pages/home/Index.vue
+++ resources/js/Pages/home/Index.vue
@@ -6,5 +6,6 @@ import {Link} from "@inertiajs/vue3";
 <template>
     <div class="min-h-screen bg-white text-center text-2xl p-6 italic">
        Sonniva is coming soon!
+       Sonniva is coming soon!
     </div>
 </template>
