6de266886e7d140f2f41fa3ecfc0553404b737ba
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-02-17 11:04:02 +0400

    add casts

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,25 +3,30 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Propaganistas\LaravelPhone\Casts\E164PhoneNumberCast;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
+
+    public $incrementing = false;
+
+    protected $keyType='string';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var list<string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
+
+    protected $appends = ['local_phone', 'international_phone'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -43,6 +48,30 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'phone' => E164PhoneNumberCast::class.':phone_country',
         ];
+    }
+
+    protected function localPhone(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->phone && $this->phone_country
+                ? phone($this->phone, $this->phone_country)
+                    ->formatForMobileDialingInCountry($this->phone_country)
+                : null
+        );
+    }
+
+    /**
+     * Cast phone in international format (with country code).
+     */
+    protected function internationalPhone(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->phone && $this->phone_country
+                ? phone($this->phone, $this->phone_country)
+                    ->formatE164()
+                : null
+        );
     }
 }
