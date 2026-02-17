3d76c3bbc6569f1fbfcd09acbfb60bf046efb469
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-02-17 11:06:39 +0400

    create admin and manager

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            [
+                'phone' => '+995574234084',
+            ],
+            [
+                'name' => 'Admin',
+                'lastname' => 'Panel',
+                'tax_id' => '12345678910',
+                'email' => 'frame@sonniva.ge',
+                'role' => 'admin',
+                'phone_country' => 'GE',
+                'phone_verified_at' => now(),
+                'email_verified_at' => now(),
+                'password' => Hash::make('SonnivaFrame2025@'),
+            ]
+        );
+        User::updateOrCreate(
+            [
+                'phone' => '+995596720001'],
+            [
+                'name' => 'Manager',
+                'lastname' => 'Panel',
+                'tax_id' => '12345678910',
+                'email' => 'manager@sonniva.ge',
+                'role' => 'manager',
+                'phone_country' => 'GE',
+                'phone_verified_at' => now(),
+                'email_verified_at' => now(),
+                'password' => Hash::make('Sonniva2025@'),
+            ]);
+    }
+}
