78798045e5b10fab9fa1d335295b313bb843bc06
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-02-17 15:54:59 +0400

    add registration api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/register/verify-phone', [RegisterController::class, 'verifyCode']);
+Route::post('/register/resend-code', [RegisterController::class, 'resendCode']);
