e24c4f4c13b433c9b548003e7c255c853cd0df52
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-02-24 14:02:15 +0400

    set guarded cols

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -10,23 +10,6 @@ class Item extends Model
 {
     use HasUuids;
 
-    protected $fillable = ['id', 'created_at', 'updated_at'];
+    protected $guarded = ['id', 'created_at', 'updated_at'];
 
-    protected function slug(): CastAttribute
-    {
-        return CastAttribute::make(
-            set: fn () => preg_replace(
-                '/\s+/u',
-                '-',
-                trim(
-                    preg_replace(
-                        '/[-]+/u',
-                        ' ',
-                        mb_strtolower($this->description, 'UTF-8')
-                    )
-                )
-            ),
-            get: fn ($value) => $value
-        );
-    }
 }
