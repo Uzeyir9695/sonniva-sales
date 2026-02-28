34849cfba68e4730dc8fbd69fd85125f1ae44d76
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-02-28 18:28:50 +0400

    add 404 page for fallback routes

---
+++ resources/js/Pages/404.vue
@@ -0,0 +1,13 @@
+<script setup>
+  defineProps({})
+</script>
+
+<template>
+<div>
+    <div class="flex flex-col items-center justify-center h-screen gap-6">
+        <h1 class="text-6xl font-bold text-gray-800">404</h1>
+        <p class="text-lg text-gray-600">Sorry, the page you are looking for could not be found.</p>
+        <a href="/" class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition">Go to Home</a>
+    </div>
+</div>
+</template>
