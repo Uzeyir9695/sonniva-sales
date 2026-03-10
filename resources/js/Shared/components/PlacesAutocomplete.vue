45a480e244fe12473547c05006a6722225ee2764
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-03-10 10:24:28 +0400

    move script at first top

--- resources/js/Shared/components/PlacesAutocomplete.vue
+++ resources/js/Shared/components/PlacesAutocomplete.vue
@@ -1,30 +1,3 @@
-<template>
-    <div class="w-full">
-        <FloatLabel variant="on">
-            <PrimeInputText id="city" class="p-2.5" v-model="query" @input="onInput" />
-            <label for="city">მისამართი <span class="text-red-500">*</span></label>
-        </FloatLabel>
-        <ul v-if="suggestions.length" class="border rounded-md bg-white mt-2 shadow-md">
-            <li
-                v-for="(item, i) in suggestions"
-                :key="i"
-                class="p-2 hover:bg-gray-100 cursor-pointer"
-                @click="selectSuggestion(item)"
-            >
-                <!-- Use structuredFormat if available, else fallback to text.text -->
-                <strong>
-                    {{ item.placePrediction.structuredFormat?.mainText?.text || item.placePrediction.text?.text }}
-                </strong>
-                <br v-if="item.placePrediction.structuredFormat?.secondaryText"/>
-                <small v-if="item.placePrediction.structuredFormat?.secondaryText">
-                    {{ item.placePrediction.structuredFormat.secondaryText.text }}
-                </small>
-            </li>
-        </ul>
-    </div>
-</template>
-
-
 <script setup>
 import { ref } from "vue";
 import PrimeInputText from '@/Pages/PrimevueComponents/PrimeInputText.vue';
@@ -88,6 +61,32 @@ function selectSuggestion(item) {
     emits("update:modelValue", query.value);
 }
 </script>
+
+<template>
+    <div class="w-full">
+        <FloatLabel variant="on">
+            <PrimeInputText id="city" class="p-2.5" v-model="query" @input="onInput" />
+            <label for="city">მისამართი <span class="text-red-500">*</span></label>
+        </FloatLabel>
+        <ul v-if="suggestions.length" class="border rounded-md bg-white mt-2 shadow-md">
+            <li
+                v-for="(item, i) in suggestions"
+                :key="i"
+                class="p-2 hover:bg-gray-100 cursor-pointer"
+                @click="selectSuggestion(item)"
+            >
+                <!-- Use structuredFormat if available, else fallback to text.text -->
+                <strong>
+                    {{ item.placePrediction.structuredFormat?.mainText?.text || item.placePrediction.text?.text }}
+                </strong>
+                <br v-if="item.placePrediction.structuredFormat?.secondaryText"/>
+                <small v-if="item.placePrediction.structuredFormat?.secondaryText">
+                    {{ item.placePrediction.structuredFormat.secondaryText.text }}
+                </small>
+            </li>
+        </ul>
+    </div>
+</template>
 
 <style scoped>
 ul {
