cc632d3015f41ea47f6295051233c37db9369043
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-03-14 07:12:09 +0400

    hide logout and lang buttons on large screen on drawer menu

--- resources/js/Shared/menu/SmallDeviceMegaMenu.vue
+++ resources/js/Shared/menu/SmallDeviceMegaMenu.vue
@@ -113,7 +113,7 @@ defineExpose({ openDrawer })
                 ref="drawerRef"
                 class="fixed top-20 left-0 z-50 min-h-[calc(100vh-80px)] w-80 bg-white shadow-2xl flex flex-col"
             >
-                <div class="bg-gray-50 shado w-xs flex items-center justify-between px-4 py-4 border-y border-gray-100">
+                <div class="bg-gray-50 shado w-xs flex items-center justify-between px-4 py-4 border-y border-gray-100 sm:hidden">
                     <WeglotSwitcher />
 
                     <LogoutButton />
