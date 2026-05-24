883a8baf6fe9573a5a91dbf71cb1e9bbaf71b428
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-05-24 10:44:21 +0400

    get most sold 25 items of some brands

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Attribute;
 use App\Models\Item;
 use Inertia\Inertia;
 
@@ -13,7 +12,12 @@ class HomeController extends Controller
     public function index()
     {
         $carouselItems = [
-            $this->getSametItems('SAMET'),
+            $this->getItemsByBrand('SAMET'),
+            $this->getItemsByBrand('DEWALT'),
+            $this->getItemsByBrand('ALBATUR'),
+            $this->getItemsByBrand('HMSFIX'),
+            $this->getItemsByBrand('EKSEN'),
+            $this->getItemsByBrand('TOLSEN'),
         ];
 
         return Inertia::render('Home/Index', [
@@ -21,18 +25,19 @@ class HomeController extends Controller
         ]);
     }
 
-    private function getSametItems(string $brand, int $limit = 25): array
+    private function getItemsByBrand(string $brand, int $limit = 25): array
     {
         $items = Item::where('inventory', '>', 0)
-            ->whereHas('attributes', fn($q) =>
-                $q->where('name', self::BRAND_ATTRIBUTE_NAME)
-                    ->where('value', $brand)
-                )
+            ->whereHas('attributes', fn ($q) => $q->where('name', self::BRAND_ATTRIBUTE_NAME)
+                ->where('value', $brand)
+            )
+            ->withSum('orderItems', 'quantity')
+            ->orderByDesc('order_items_sum_quantity')
             ->take($limit)
             ->get();
 
-        return  [
-            'title' => 'SAMET',
+        return [
+            'title' => $brand,
             'items' => $items,
         ];
     }
