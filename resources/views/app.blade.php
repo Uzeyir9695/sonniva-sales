4820a3e6d843306ff292d1bd3882e1203077ddf3
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-06-09 17:44:16 +0400

    comment the translator out

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -29,16 +29,16 @@
 
 {{--        GlobalSeo Service --}}
         <link href="https://unpkg.com/globalseo/dist/translate.css" rel="stylesheet">
-        <script
-            defer
-            src="https://unpkg.com/globalseo/dist/translate.js"
-            data-globalseo-key="55350ca0-f788-4234-bf81-9a40769e6094"
-            data-use-browser-language="false"
-            data-original-language="ka"
-            data-translation-mode="searchParams"
-            data-allowed-languages="en, tr"
-            data-exclude-classes=""
-        ></script>
+{{--        <script--}}
+{{--            defer--}}
+{{--            src="https://unpkg.com/globalseo/dist/translate.js"--}}
+{{--            data-globalseo-key="55350ca0-f788-4234-bf81-9a40769e6094"--}}
+{{--            data-use-browser-language="false"--}}
+{{--            data-original-language="ka"--}}
+{{--            data-translation-mode="searchParams"--}}
+{{--            data-allowed-languages="en, tr"--}}
+{{--            data-exclude-classes=""--}}
+{{--        ></script>--}}
 
 {{--        <script type="text/javascript" src="https://cdn.weglot.com/weglot.min.js"></script>--}}
 {{--        <script>--}}
