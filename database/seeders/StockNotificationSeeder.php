95446fcdd405e4bac22b94ee0302d2266eacaec0
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-06-25 10:58:30 +0400

    subscribe items not in stock for specific user temporary

---
+++ database/seeders/StockNotificationSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Item;
+use App\Models\StockNotification;
+use Illuminate\Database\Seeder;
+
+class StockNotificationSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $userId = '019ed9ca-bc2f-7015-b01f-4067033543a6';
+
+        Item::query()
+            ->where('inventory', 0)
+            ->limit(400)
+            ->get()
+            ->each(function (Item $item) use ($userId) {
+                StockNotification::firstOrCreate([
+                    'user_id' => $userId,
+                    'item_id' => $item->id,
+                ]);
+            });
+    }
+}
