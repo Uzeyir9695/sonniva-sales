1366e012ab4bb7f625aab001b3b06d84d9f7d54c
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-06-29 20:58:08 +0400

    rerun items seeder

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -17,10 +17,10 @@ class ItemSeeder extends Seeder
 
     public function run(): void
     {
-        //                DB::statement('SET FOREIGN_KEY_CHECKS=0');
-        //                Attribute::truncate();
-        //                Item::truncate();
-        //                DB::statement('SET FOREIGN_KEY_CHECKS=1');
+                        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+                        Attribute::truncate();
+                        Item::truncate();
+                        DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
         $startedAt = now();
 
