14212498b24d4d94560988159b57a96a51d4267c
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-07-05 13:04:04 +0400

    build

---
+++ public/build/assets/Register-D6K9UTJ-.js
@@ -0,0 +1,164 @@
+import{a as K,m as D,aY as U,ab as j,aK as C,aG as H,bm as L,aw as f,a3 as m,a0 as i,aE as I,ap as W,an as k,a2 as T,b1 as w,ai as _,ax as V,aF as Y,h as $,aD as E,a1 as R,a4 as J,bl as s,w as Q,bb as X,aC as B,b8 as Z,a7 as r,b3 as d,ag as tt,bo as et,aW as nt,aU as ot,i as lt,aL as at,a6 as it,al as rt}from"./app-DcWfj8AN.js";import{s as ut}from"./index-CFNjL8AZ.js";import{s as st,a as dt,b as pt}from"./index-BbB1Zy2e.js";/* empty css            */var gt=`
+    .p-togglebutton {
+        display: inline-flex;
+        cursor: pointer;
+        user-select: none;
+        overflow: hidden;
+        position: relative;
+        color: dt('togglebutton.color');
+        background: dt('togglebutton.background');
+        border: 1px solid dt('togglebutton.border.color');
+        padding: dt('togglebutton.padding');
+        font-size: 1rem;
+        font-family: inherit;
+        font-feature-settings: inherit;
+        transition:
+            background dt('togglebutton.transition.duration'),
+            color dt('togglebutton.transition.duration'),
+            border-color dt('togglebutton.transition.duration'),
+            outline-color dt('togglebutton.transition.duration'),
+            box-shadow dt('togglebutton.transition.duration');
+        border-radius: dt('togglebutton.border.radius');
+        outline-color: transparent;
+        font-weight: dt('togglebutton.font.weight');
+    }
+
+    .p-togglebutton-content {
+        display: inline-flex;
+        flex: 1 1 auto;
+        align-items: center;
+        justify-content: center;
+        gap: dt('togglebutton.gap');
+        padding: dt('togglebutton.content.padding');
+        background: transparent;
+        border-radius: dt('togglebutton.content.border.radius');
+        transition:
+            background dt('togglebutton.transition.duration'),
+            color dt('togglebutton.transition.duration'),
+            border-color dt('togglebutton.transition.duration'),
+            outline-color dt('togglebutton.transition.duration'),
+            box-shadow dt('togglebutton.transition.duration');
+    }
+
+    .p-togglebutton:not(:disabled):not(.p-togglebutton-checked):hover {
+        background: dt('togglebutton.hover.background');
+        color: dt('togglebutton.hover.color');
+    }
+
+    .p-togglebutton.p-togglebutton-checked {
+        background: dt('togglebutton.checked.background');
+        border-color: dt('togglebutton.checked.border.color');
+        color: dt('togglebutton.checked.color');
+    }
+
+    .p-togglebutton-checked .p-togglebutton-content {
+        background: dt('togglebutton.content.checked.background');
+        box-shadow: dt('togglebutton.content.checked.shadow');
+    }
+
+    .p-togglebutton:focus-visible {
+        box-shadow: dt('togglebutton.focus.ring.shadow');
+        outline: dt('togglebutton.focus.ring.width') dt('togglebutton.focus.ring.style') dt('togglebutton.focus.ring.color');
+        outline-offset: dt('togglebutton.focus.ring.offset');
+    }
+
+    .p-togglebutton.p-invalid {
+        border-color: dt('togglebutton.invalid.border.color');
+    }
+
+    .p-togglebutton:disabled {
+        opacity: 1;
+        cursor: default;
+        background: dt('togglebutton.disabled.background');
+        border-color: dt('togglebutton.disabled.border.color');
+        color: dt('togglebutton.disabled.color');
+    }
+
+    .p-togglebutton-label,
+    .p-togglebutton-icon {
+        position: relative;
+        transition: none;
+    }
+
+    .p-togglebutton-icon {
+        color: dt('togglebutton.icon.color');
+    }
+
+    .p-togglebutton:not(:disabled):not(.p-togglebutton-checked):hover .p-togglebutton-icon {
+        color: dt('togglebutton.icon.hover.color');
+    }
+
+    .p-togglebutton.p-togglebutton-checked .p-togglebutton-icon {
+        color: dt('togglebutton.icon.checked.color');
+    }
+
+    .p-togglebutton:disabled .p-togglebutton-icon {
+        color: dt('togglebutton.icon.disabled.color');
+    }
+
+    .p-togglebutton-sm {
+        padding: dt('togglebutton.sm.padding');
+        font-size: dt('togglebutton.sm.font.size');
+    }
+
+    .p-togglebutton-sm .p-togglebutton-content {
+        padding: dt('togglebutton.content.sm.padding');
+    }
+
+    .p-togglebutton-lg {
+        padding: dt('togglebutton.lg.padding');
+        font-size: dt('togglebutton.lg.font.size');
+    }
+
+    .p-togglebutton-lg .p-togglebutton-content {
+        padding: dt('togglebutton.content.lg.padding');
+    }
+
+    .p-togglebutton-fluid {
+        width: 100%;
+    }
+`,bt={root:function(e){var o=e.instance,u=e.props;return["p-togglebutton p-component",{"p-togglebutton-checked":o.active,"p-invalid":o.$invalid,"p-togglebutton-fluid":u.fluid,"p-togglebutton-sm p-inputfield-sm":u.size==="small","p-togglebutton-lg p-inputfield-lg":u.size==="large"}]},content:"p-togglebutton-content",icon:"p-togglebutton-icon",label:"p-togglebutton-label"},ct=K.extend({name:"togglebutton",style:gt,classes:bt}),ft={name:"BaseToggleButton",extends:U,props:{onIcon:String,offIcon:String,onLabel:{type:String,default:"Yes"},offLabel:{type:String,default:"No"},readonly:{type:Boolean,default:!1},tabindex:{type:Number,default:null},ariaLabelledby:{type:String,default:null},ariaLabel:{type:String,default:null},size:{type:String,default:null},fluid:{type:Boolean,default:null}},style:ct,provide:function(){return{$pcToggleButton:this,$parentInstance:this}}};function S(t){"@babel/helpers - typeof";return S=typeof Symbol=="function"&&typeof Symbol.iterator=="symbol"?function(e){return typeof e}:function(e){return e&&typeof Symbol=="function"&&e.constructor===Symbol&&e!==Symbol.prototype?"symbol":typeof e},S(t)}function mt(t,e,o){return(e=yt(e))in t?Object.defineProperty(t,e,{value:o,enumerable:!0,configurable:!0,writable:!0}):t[e]=o,t}function yt(t){var e=vt(t,"string");return S(e)=="symbol"?e:e+""}function vt(t,e){if(S(t)!="object"||!t)return t;var o=t[Symbol.toPrimitive];if(o!==void 0){var u=o.call(t,e);if(S(u)!="object")return u;throw new TypeError("@@toPrimitive must return a primitive value.")}return(e==="string"?String:Number)(t)}var F={name:"ToggleButton",extends:ft,inheritAttrs:!1,emits:["change"],methods:{getPTOptions:function(e){var o=e==="root"?this.ptmi:this.ptm;return o(e,{context:{active:this.active,disabled:this.disabled}})},onChange:function(e){!this.disabled&&!this.readonly&&(this.writeValue(!this.d_value,e),this.$emit("change",e))},onBlur:function(e){var o,u;(o=(u=this.formField).onBlur)===null||o===void 0||o.call(u,e)}},computed:{active:function(){return this.d_value===!0},hasLabel:function(){return C(this.onLabel)&&C(this.offLabel)},label:function(){return this.hasLabel?this.d_value?this.onLabel:this.offLabel:" "},dataP:function(){return j(mt({checked:this.active,invalid:this.$invalid},this.size,this.size))}},directives:{ripple:D}},ht=["tabindex","disabled","aria-pressed","aria-label","aria-labelledby","data-p-checked","data-p-disabled","data-p"],kt=["data-p"];function wt(t,e,o,u,g,a){var l=H("ripple");return L((f(),m("button",k({type:"button",class:t.cx("root"),tabindex:t.tabindex,disabled:t.disabled,"aria-pressed":t.d_value,onClick:e[0]||(e[0]=function(){return a.onChange&&a.onChange.apply(a,arguments)}),onBlur:e[1]||(e[1]=function(){return a.onBlur&&a.onBlur.apply(a,arguments)})},a.getPTOptions("root"),{"aria-label":t.ariaLabel,"aria-labelledby":t.ariaLabelledby,"data-p-checked":a.active,"data-p-disabled":t.disabled,"data-p":a.dataP}),[i("span",k({class:t.cx("content")},a.getPTOptions("content"),{"data-p":a.dataP}),[I(t.$slots,"default",{},function(){return[I(t.$slots,"icon",{value:t.d_value,class:W(t.cx("icon"))},function(){return[t.onIcon||t.offIcon?(f(),m("span",k({key:0,class:[t.cx("icon"),t.d_value?t.onIcon:t.offIcon]},a.getPTOptions("icon")),null,16)):T("",!0)]}),i("span",k({class:t.cx("label")},a.getPTOptions("label")),w(a.label),17)]})],16,kt)],16,ht)),[[l]])}F.render=wt;var xt=`
+    .p-selectbutton {
+        display: inline-flex;
+        user-select: none;
+        vertical-align: bottom;
+        outline-color: transparent;
+        border-radius: dt('selectbutton.border.radius');
+    }
+
+    .p-selectbutton .p-togglebutton {
+        border-radius: 0;
+        border-width: 1px 1px 1px 0;
+    }
+
+    .p-selectbutton .p-togglebutton:focus-visible {
+        position: relative;
+        z-index: 1;
+    }
+
+    .p-selectbutton .p-togglebutton:first-child {
+        border-inline-start-width: 1px;
+        border-start-start-radius: dt('selectbutton.border.radius');
+        border-end-start-radius: dt('selectbutton.border.radius');
+    }
+
+    .p-selectbutton .p-togglebutton:last-child {
+        border-start-end-radius: dt('selectbutton.border.radius');
+        border-end-end-radius: dt('selectbutton.border.radius');
+    }
+
+    .p-selectbutton.p-invalid {
+        outline: 1px solid dt('selectbutton.invalid.border.color');
+        outline-offset: 0;
+    }
+
+    .p-selectbutton-fluid {
+        width: 100%;
+    }
+    
+    .p-selectbutton-fluid .p-togglebutton {
+        flex: 1 1 0;
+    }
+`,St={root:function(e){var o=e.props,u=e.instance;return["p-selectbutton p-component",{"p-invalid":u.$invalid,"p-selectbutton-fluid":o.fluid}]}},Vt=K.extend({name:"selectbutton",style:xt,classes:St}),Ot={name:"BaseSelectButton",extends:U,props:{options:Array,optionLabel:null,optionValue:null,optionDisabled:null,multiple:Boolean,allowEmpty:{type:Boolean,default:!0},dataKey:null,ariaLabelledby:{type:String,default:null},size:{type:String,default:null},fluid:{type:Boolean,default:null}},style:Vt,provide:function(){return{$pcSelectButton:this,$parentInstance:this}}};function _t(t,e){var o=typeof Symbol<"u"&&t[Symbol.iterator]||t["@@iterator"];if(!o){if(Array.isArray(t)||(o=N(t))||e){o&&(t=o);var u=0,g=function(){};return{s:g,n:function(){return u>=t.length?{done:!0}:{done:!1,value:t[u++]}},e:function(n){throw n},f:g}}throw new TypeError(`Invalid attempt to iterate non-iterable instance.
+In order to be iterable, non-array objects must have a [Symbol.iterator]() method.`)}var a,l=!0,p=!1;return{s:function(){o=o.call(t)},n:function(){var n=o.next();return l=n.done,n},e:function(n){p=!0,a=n},f:function(){try{l||o.return==null||o.return()}finally{if(p)throw a}}}}function Bt(t){return Tt(t)||It(t)||N(t)||Lt()}function Lt(){throw new TypeError(`Invalid attempt to spread non-iterable instance.
+In order to be iterable, non-array objects must have a [Symbol.iterator]() method.`)}function N(t,e){if(t){if(typeof t=="string")return P(t,e);var o={}.toString.call(t).slice(8,-1);return o==="Object"&&t.constructor&&(o=t.constructor.name),o==="Map"||o==="Set"?Array.from(t):o==="Arguments"||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(o)?P(t,e):void 0}}function It(t){if(typeof Symbol<"u"&&t[Symbol.iterator]!=null||t["@@iterator"]!=null)return Array.from(t)}function Tt(t){if(Array.isArray(t))return P(t)}function P(t,e){(e==null||e>t.length)&&(e=t.length);for(var o=0,u=Array(e);o<e;o++)u[o]=t[o];return u}var q={name:"SelectButton",extends:Ot,inheritAttrs:!1,emits:["change"],methods:{getOptionLabel:function(e){return this.optionLabel?V(e,this.optionLabel):e},getOptionValue:function(e){return this.optionValue?V(e,this.optionValue):e},getOptionRenderKey:function(e){return this.dataKey?V(e,this.dataKey):this.getOptionLabel(e)},isOptionDisabled:function(e){return this.optionDisabled?V(e,this.optionDisabled):!1},isOptionReadonly:function(e){if(this.allowEmpty)return!1;var o=this.isSelected(e);return this.multiple?o&&this.d_value.length===1:o},onOptionSelect:function(e,o,u){var g=this;if(!(this.disabled||this.isOptionDisabled(o)||this.isOptionReadonly(o))){var a=this.isSelected(o),l=this.getOptionValue(o),p;if(this.multiple)if(a){if(p=this.d_value.filter(function(c){return!_(c,l,g.equalityKey)}),!this.allowEmpty&&p.length===0)return}else p=this.d_value?[].concat(Bt(this.d_value),[l]):[l];else{if(a&&!this.allowEmpty)return;p=a?null:l}this.writeValue(p,e),this.$emit("change",{event:e,value:p})}},isSelected:function(e){var o=!1,u=this.getOptionValue(e);if(this.multiple){if(this.d_value){var g=_t(this.d_value),a;try{for(g.s();!(a=g.n()).done;){var l=a.value;if(_(l,u,this.equalityKey)){o=!0;break}}}catch(p){g.e(p)}finally{g.f()}}}else o=_(this.d_value,u,this.equalityKey);return o}},computed:{equalityKey:function(){return this.optionValue?null:this.dataKey},dataP:function(){return j({invalid:this.$invalid})}},directives:{ripple:D},components:{ToggleButton:F}},$t=["aria-labelledby","data-p"];function Pt(t,e,o,u,g,a){var l=Y("ToggleButton");return f(),m("div",k({class:t.cx("root"),role:"group","aria-labelledby":t.ariaLabelledby},t.ptmi("root"),{"data-p":a.dataP}),[(f(!0),m($,null,E(t.options,function(p,c){return f(),R(l,{key:a.getOptionRenderKey(p),modelValue:a.isSelected(p),onLabel:a.getOptionLabel(p),offLabel:a.getOptionLabel(p),disabled:t.disabled||a.isOptionDisabled(p),unstyled:t.unstyled,size:t.size,readonly:a.isOptionReadonly(p),onChange:function(O){return a.onOptionSelect(O,p,c)},pt:t.ptm("pcToggleButton")},J({_:2},[t.$slots.option?{name:"default",fn:s(function(){return[I(t.$slots,"option",{option:p,index:c},function(){return[i("span",k({ref_for:!0},t.ptm("pcToggleButton").label),w(a.getOptionLabel(p)),17)]})]}),key:"0"}:void 0]),1032,["modelValue","onLabel","offLabel","disabled","unstyled","size","readonly","onChange","pt"])}),128))],16,$t)}q.render=Pt;const zt={class:"w-full max-w-md mx-auto my-6"},At={class:"bg-white mx-3 rounded-xl shadow-lg transition-shadow duration-500 ease-in-out border transiton-all border-gray-200 p-8"},Ct={class:"flex justify-center my-4"},Kt={key:0,class:"mb-6 bg-red-50 border border-red-200 rounded-lg p-4 flex"},Dt={class:"text-red-600 text-sm"},Ut={for:"name"},jt={for:"id-number"},Et={class:"flex justify-center gap-x-2 text-sm"},Rt={class:"flex items-center w-fit gap-x-2 text-nowrap"},Ft={__name:"Register",props:["errors"],setup(t){const o=X().props.recaptcha_site_key,u=B([{key:"individual",value:"ფიზიკური პირი"},{key:"legal_entity",value:"იურიდიული პირი"}]),g=B({key:"individual",value:"ფიზიკური პირი"}),a=B("GE"),l=Z({user_type:null,name:null,lastname:null,phone_country:a.value,phone:null,tax_id:null,email:null,password:null,password_confirmation:null,captcha_token:null});async function p(){l.captcha_token=await grecaptcha.enterprise.execute(o,{action:"signup"}),g.value?.key!=="individual"&&delete l.lastname,l.transform(c=>({...c,user_type:g.value?g.value?.key:null})).post(route("register"),{preserveState:!0})}return(c,n)=>{const O=q,y=st,x=ot,v=nt,h=dt,z=ut,G=at,A=lt;return f(),m($,null,[r(d(tt),null,{default:s(()=>[...n[8]||(n[8]=[i("title",null,"რეგისტრაცია",-1)])]),_:1}),i("div",zt,[i("div",At,[n[25]||(n[25]=i("h1",{class:"text-2xl font-bold text-gray-800 text-center"},"რეგისტრაცია",-1)),i("div",Ct,[r(O,{modelValue:g.value,"onUpdate:modelValue":n[0]||(n[0]=b=>g.value=b),optionLabel:"key","allow-empty":!1,options:u.value},{option:s(b=>[i("p",null,w(b.option.value),1)]),_:1},8,["modelValue","options"])]),Object.keys(t.errors).length>0?(f(),m("div",Kt,[n[9]||(n[9]=i("i",{class:"pi pi-exclamation-circle text-red-400 mr-3 mt-0.5"},null,-1)),i("div",Dt,[(f(!0),m($,null,E(t.errors,(b,M)=>(f(),m("p",{key:M},w(b),1))),128))])])):T("",!0),i("form",{onSubmit:et(p,["prevent"]),class:"flex flex-col gap-6"},[r(h,null,{default:s(()=>[r(y,null,{default:s(()=>[...n[10]||(n[10]=[i("i",{class:"pi pi-user text-gray-400"},null,-1)])]),_:1}),r(v,{variant:"on"},{default:s(()=>[r(x,{id:"name",modelValue:d(l).name,"onUpdate:modelValue":n[1]||(n[1]=b=>d(l).name=b),invalid:!!d(l).errors.name},null,8,["modelValue","invalid"]),i("label",Ut,w(g.value?.key==="individual"?"სახელი":"კომპანიის სახელი"),1)]),_:1})]),_:1}),g.value?.key==="individual"?(f(),R(h,{key:0},{default:s(()=>[r(y,null,{default:s(()=>[...n[11]||(n[11]=[i("i",{class:"pi pi-users text-gray-400"},null,-1)])]),_:1}),r(v,{variant:"on"},{default:s(()=>[r(x,{id:"lastname",modelValue:d(l).lastname,"onUpdate:modelValue":n[2]||(n[2]=b=>d(l).lastname=b),invalid:!!d(l).errors.lastname},null,8,["modelValue","invalid"]),n[12]||(n[12]=i("label",{for:"lastname"}," გვარი ",-1))]),_:1})]),_:1})):T("",!0),r(h,null,{default:s(()=>[r(y,null,{default:s(()=>[...n[13]||(n[13]=[i("i",{class:"pi pi-mobile text-gray-400"},null,-1)])]),_:1}),r(v,{variant:"on"},{default:s(()=>[L(r(x,{id:"phone",modelValue:d(l).phone,"onUpdate:modelValue":n[3]||(n[3]=b=>d(l).phone=b),invalid:!!d(l).errors.phone},null,8,["modelValue","invalid"]),[[A,{pattern:/[\d+]+$/,validateOnly:!0}]]),n[14]||(n[14]=i("label",{for:"phone"}," ტელეფონი ",-1))]),_:1})]),_:1}),r(h,null,{default:s(()=>[r(y,null,{default:s(()=>[...n[15]||(n[15]=[i("i",{class:"pi pi-id-card text-gray-400"},null,-1)])]),_:1}),r(v,{variant:"on"},{default:s(()=>[L(r(x,{id:"id-number",modelValue:d(l).tax_id,"onUpdate:modelValue":n[4]||(n[4]=b=>d(l).tax_id=b),maxlength:11,invalid:!!d(l).errors.tax_id},null,8,["modelValue","invalid"]),[[A,{pattern:/[\d+]+$/,validateOnly:!0}]]),i("label",jt,w(g.value?.key==="individual"?"პირადი ნომერი":"საიდენტიფიკაციო ნომერი"),1)]),_:1})]),_:1}),r(h,null,{default:s(()=>[r(y,null,{default:s(()=>[...n[16]||(n[16]=[i("i",{class:"pi pi-envelope text-gray-400"},null,-1)])]),_:1}),r(v,{variant:"on"},{default:s(()=>[r(x,{id:"email",modelValue:d(l).email,"onUpdate:modelValue":n[5]||(n[5]=b=>d(l).email=b),invalid:!!d(l).errors.email},null,8,["modelValue","invalid"]),n[17]||(n[17]=i("label",{for:"email"},"ელ. ფოსტა",-1))]),_:1})]),_:1}),r(h,null,{default:s(()=>[r(y,null,{default:s(()=>[...n[18]||(n[18]=[i("i",{class:"pi pi-lock text-gray-400"},null,-1)])]),_:1}),r(v,{variant:"on"},{default:s(()=>[r(z,{modelValue:d(l).password,"onUpdate:modelValue":n[6]||(n[6]=b=>d(l).password=b),inputId:"password",toggleMask:"","pt:maskIcon:class":"z-10","pt:unmaskIcon:class":"z-10",feedback:!1,invalid:!!d(l).errors.password},null,8,["modelValue","invalid"]),n[19]||(n[19]=i("label",{for:"password"}," პაროლი ",-1))]),_:1})]),_:1}),r(h,null,{default:s(()=>[r(y,null,{default:s(()=>[...n[20]||(n[20]=[i("i",{class:"pi pi-lock text-gray-400"},null,-1)])]),_:1}),r(v,{variant:"on"},{default:s(()=>[r(z,{modelValue:d(l).password_confirmation,"onUpdate:modelValue":n[7]||(n[7]=b=>d(l).password_confirmation=b),inputId:"password_confirm",toggleMask:"","pt:maskIcon:class":"z-10","pt:unmaskIcon:class":"z-10",feedback:!1,invalid:!!d(l).errors.password_confirmation},null,8,["modelValue","invalid"]),n[21]||(n[21]=i("label",{for:"password_confirm"}," გაიმეორე პაროლი ",-1))]),_:1})]),_:1}),n[24]||(n[24]=i("p",null," თუ გაქვთ მცირე მეწარმის სტატუსი ან ხართ იურიდიული პირი, სპეციალური შეთავაზებების მისაღებად დაგვიკავშირდით ",-1)),i("div",null,[r(G,{type:"submit",icon:"pi pi-user-plus",label:"დადასტურება",class:"w-full bg-blue-500 hover:bg-blue-500/90 border-none text-white rounded-lg py-2.5"})]),r(d(pt),{"pt:root:class":"m-0"}),i("div",null,[i("div",Et,[n[23]||(n[23]=i("p",{class:"w-fit"},"უკვე ხარ რეგისტრირებული?",-1)),i("div",Rt,[r(d(rt),{href:c.route("login"),class:"flex items-center gap-x-1 text-brand-500 text-sm no-underline"},{default:s(()=>[...n[22]||(n[22]=[i("i",{class:"pi pi-user text-brand-500"},null,-1),it(" შესვლა ",-1)])]),_:1},8,["href"])])])])],32)])])],64)}}},Ht=Q(Ft,[["__scopeId","data-v-d96181df"]]);export{Ht as default};
