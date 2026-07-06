c21a1baaf5aa743fa0e1bc9eeb8a0c70bc407c04
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-07-06 12:15:11 +0400

    integrate LiveCaller Chat

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -65,5 +65,23 @@
     </head>
     <body class="font-sans antialiased bg-gray-50">
         @inertia
+
+        <script>
+            (function(w,t,c,p,s,e,l,k){
+                p=new Promise(function(r){w[c]={client:function(){return p}};
+                    l=document.createElement('div');l.setAttribute("id", "live-caller-widget");
+                    s=document.createElement(t);s.async=1;s.setAttribute("data-livecaller", 'script');
+                    k=document.body || document.documentElement;k.insertBefore(l, k.firstChild);
+                    l.setAttribute("data-livecaller", 'mount-el');s.src='https://cdn.livecaller.io/js/app.js';
+                    e=document.getElementsByTagName(t)[0];e ? e.parentNode.insertBefore(s,e) : k.insertBefore(s, l.nextSibling);
+                    s.onload=function(){r(w[c]);};});return p;
+            })(window,'script','LiveCaller').then(function(){
+                try{
+                    LiveCaller.config.merge({"widget":{"id":"f659b235-e91a-445d-a2b7-a8190574a76b"},"app":{"locale":"ka"}});
+                    LiveCaller.liftOff();
+                }catch(e){
+                }
+            });
+        </script>
     </body>
 </html>
