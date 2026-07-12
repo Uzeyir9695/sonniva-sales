7417a92880636d89fc085ba23ddf116f0c41597c
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-07-12 16:17:45 +0400

    run scheduler commands between 9am and 6pm

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,16 @@
 <?php
 
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-//Schedule::command('sitemap:generate')->daily();
-Schedule::command('inventory:sync')->everyFiveMinutes()->withoutOverlapping();
+// Schedule::command('sitemap:generate')->daily();
+Schedule::command('inventory:sync')
+    ->everyFiveMinutes()
+    ->between('09:00', '18:00')
+    ->timezone('Asia/Tbilisi')
+    ->withoutOverlapping();
+
+Schedule::command('items:sync-data')
+    ->everyThirtyMinutes()
+    ->between('09:00', '18:00')
+    ->timezone('Asia/Tbilisi')
+    ->withoutOverlapping();
