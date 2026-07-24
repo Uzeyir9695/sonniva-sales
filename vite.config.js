108601803cca6b38aa60827e7719571bbacffdaa
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-07-24 17:01:56 +0400

    revert swiper

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
         tailwindcss(),
     ],
     ssr: {
-        noExternal: ['primevue', 'swiper'],
+        noExternal: ['primevue'],
     },
     resolve: {
         alias: {
