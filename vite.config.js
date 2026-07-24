e3966c2027119a4efc3c560693809f71e5776d4c
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-07-24 16:52:41 +0400

    exclude swiper

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
         tailwindcss(),
     ],
     ssr: {
-        noExternal: ['primevue'],
+        noExternal: ['primevue', 'swiper'],
     },
     resolve: {
         alias: {
