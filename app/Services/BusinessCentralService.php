828aed824b2174a036c7279d7b46e0d8c691bc73
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2026-07-31 18:57:17 +0400

    set salesperson code to WEB ALU

--- app/Services/BusinessCentralService.php
+++ app/Services/BusinessCentralService.php
@@ -276,7 +276,7 @@ class BusinessCentralService
             'Document_Type' => 'Order',
             'Sell_to_Customer_No' => $user->bc_customer_no,
             'External_Document_No' => $order->invoice_no,
-            'Salesperson_Code' => '6002',
+            'Salesperson_Code' => 'WEB ALU',
             'ShippingOptions' => 'Custom Address',
             'Ship_to_Name' => $user->name,
             'Ship_to_Address' => $order->address.
